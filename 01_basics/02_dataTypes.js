fbc2f34cff10196004d53ad4273444bebf22886a
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-09-24 13:32:00 +0000

    basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+// Datatypes and ECMA Standards
+
+"use strict" // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+console.log("Hitesh")
+

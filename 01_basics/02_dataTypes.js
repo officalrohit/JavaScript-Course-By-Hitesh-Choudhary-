651a251043293f74136523ec1a87784b29b64640
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-09-24 15:57:32 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -10,3 +10,19 @@ console.log(3
 
 console.log("Hitesh")
 
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => ture/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// Objects
+
+console.log(typeof undefined);
+

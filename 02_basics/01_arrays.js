1776fee7d49fcb6753a24da61e946fd546116248
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-09-26 19:41:18 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// array
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(0, 1, 2, 3, 4)
+
+// console.log(myArray[0]);
+
+// Array methods
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3));
+
+// const newArr = myArray.join()
+
+// console.log(myArray);
+// console.log(typeof newArr);
+
+// slice, splice
+
+console.log("A ", myArray);
+
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArray);
+
+const myn2 = myArray.splice(1,3)
+console.log("C", myArray);
+console.log(myn2);

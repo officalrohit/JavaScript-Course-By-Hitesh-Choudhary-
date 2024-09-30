9917cad51d557153b400d3413b23d168551f4082
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-09-30 10:31:04 +0000

    To do List

--- Practice/script.js
+++ Practice/script.js
@@ -1,10 +1,13 @@
-const inputValue = document.querySelector('.input-task').value
-const addButton = document.querySelector('.add-btn').addEventListener('click', function () {
-    if (inputValue = "") {
-        alert('please enter your task')
-    } else {
-        // const task = document.querySelector('.task').innerHTML += `<span>${inputValue}</span>`
-        console.log(inputValue);
-        
-    }
+const addBtn = document.querySelector(".add-btn")
+
+addBtn.addEventListener('click', function () {
+    const inputTask = document.querySelector(".input-task").value
+    console.log(inputTask);
+    const task = document.querySelector(".task").innerHTML = `<span>${inputTask}</span> <button class="delete"><i class="fa-solid fa-trash-can"></i></button>`
+})
+
+const deletetask = document.querySelector(".delete")
+
+deletetask.addEventListener('click', function () {
+    const task = document.querySelector(".task").textContent = ''
 })

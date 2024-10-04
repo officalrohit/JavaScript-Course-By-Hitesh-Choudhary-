a828d444b9147ab255ea83833a63641ab217d61b
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-10-04 19:36:14 +0000

    to do list

--- Practice/script.js
+++ Practice/script.js
@@ -3,5 +3,6 @@ const addButton = document.querySelector(".add-btn")
 addButton.addEventListener('click', function () {
     const inputTask = document.querySelector(".input-task").value
     console.log(inputTask);
+    const taskContainer = document.querySelector(".tasks-container").innerHTML = (`<div class="task"><span>${inputTask}</span><button class="delete-btn">delete</button>`)
     
 })

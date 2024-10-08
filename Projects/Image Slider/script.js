b5533a2038ec3d86618ba99f890a166a7014a716
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-10-08 07:50:20 +0000

    image slider

---
+++ Projects/Image Slider/script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container")
+const cardsContainer = document.querySelectorAll("card")
+const preButton = document.querySelector("pre-btn")
+const nextButton = document.querySelector(".next-btn")
+
+nextButton.addEventListener('click', function () {
+    container.forEach(() => {
+        container.style = background
+    });
+})

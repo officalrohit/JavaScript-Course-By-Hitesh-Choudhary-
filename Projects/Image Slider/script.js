fc210bea6dede756dfe732afe4a35e1f4cd3b685
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-10-09 12:38:04 +0000

    image slider

--- Projects/Image Slider/script.js
+++ Projects/Image Slider/script.js
@@ -1,7 +1,12 @@
-const nextBtn = document.querySelector(".next")
-const prevBtn = document.querySelector(".prev")
+let nextBtn = document.querySelector(".next")
+let prevBtn = document.querySelector(".prev")
 
 nextBtn.addEventListener('click', function () {
-    const items = document.querySelectorAll(".item")
+    let items = document.querySelectorAll(".item")
     document.querySelector(".slide").appendChild(items[0])
+})
+
+prevBtn.addEventListener('click', function () {
+    let items = document.querySelectorAll(".item")
+    document.querySelector(".slide").prepend(items[items.length - 1])
 })

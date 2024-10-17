bc70b3798189818597e96ba69aadef8b7f1cd706
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-10-17 18:57:47 +0000

    oops

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -29,5 +29,20 @@ const user = {
 //console.log(user.getUserDetails());
 // console.log(this);
 
-const promiseOne = new Promise()
-const date = new Date()
+function User(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function () {
+        console.log(`Welcome ${this.username}`);
+        
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+// console.log(userTwo);

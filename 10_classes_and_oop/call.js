eb4f38e8a8b70392eba22d72498f39e66865f228
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-10-22 00:51:07 +0000

    call

--- 10_classes_and_oop/call.js
+++ 10_classes_and_oop/call.js
@@ -5,7 +5,7 @@ function SetUsername(username) {
 }
 
 function createUser(username, email, password) {
-    SetUsername.call(username)
+    SetUsername.call(this,username)
 
     this.email = email
     this.password = password

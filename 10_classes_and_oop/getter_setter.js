37291770a12dc3c4d3931902f572e677ac4ae821
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-10-24 17:21:46 +0000

    getter and setter

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -1,21 +1,23 @@
-class User {
+class User{
     constructor(email, password) {
         this.email = email;
-        this.password = password
+        this.password = password;
     }
 
     get email() {
-
-        
-    }
-    get password () {
-        return `${this._password}hitsh`
+        return this._email.toUpperCase()
     }
 
+    set email(value) {
+        this._email = value
+        }
+    get password() {
+        return `${this._password}hitesh`
+    }
     set password(value) {
         this._password = value
     }
 }
 
 const hitesh = new User("h@hitesh.ai", "abc")
-console.log(hitesh.password);
+console.log(hitesh.email);

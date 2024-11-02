71bfd64668f00c1bfe93cb4a5e6759a59523f3a7
rohitcreates <167241336+rohitcreates@users.noreply.github.com>
2024-11-02 17:55:57 +0000

    array

--- 11_fun_with_js/test.js
+++ 11_fun_with_js/test.js
@@ -1,3 +1,17 @@
 const myArr = []
 
 %DebugPrint(myArr)
+
+// continious, Holey
+
+// SMI (small interger)
+// Packed element
+// Double (float, sting, function)
+
+const arrTwo = [1, 2, 3, 4, 5];
+// Packed_SMI_Element
+arrTwo.push(6.0)
+// Packed_Double_Elements
+
+arrTwo.push(7)
+// Packed_Elements
